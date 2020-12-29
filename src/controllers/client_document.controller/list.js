78fbb63187b4b6ea290d0d724e6685fc512c74b0
Lefcott <leandro.cotti@widergy.com>
2020-12-29 19:28:48 -0300

    adjust endpoint

--- src/controllers/client_document.controller/list.js
+++ src/controllers/client_document.controller/list.js
@@ -2,7 +2,7 @@ import ClientDocument from '../../models/ClientDocument';
 
 /** @param {import('express').Request} req @param {import('express').Response} res */
 export default async ({ query }, res) => {
-  const client_documents = await ClientDocument.find(query);
+  const client_documents = await ClientDocument.find(query).map(({ value }) => value);
 
   res.status(200).json(client_documents);
 };
