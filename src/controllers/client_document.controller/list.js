cc250620229ae384be3ec4c236c68e4b8bb07487
Lefcott <leandro.cotti@widergy.com>
2020-12-29 19:41:08 -0300

    change professionals endpoint

--- src/controllers/client_document.controller/list.js
+++ src/controllers/client_document.controller/list.js
@@ -2,7 +2,7 @@ import ClientDocument from '../../models/ClientDocument';
 
 /** @param {import('express').Request} req @param {import('express').Response} res */
 export default async ({ query }, res) => {
-  const client_documents = await ClientDocument.find(query).map(({ value }) => value);
+  const client_documents = await ClientDocument.find(query);
 
-  res.status(200).json(client_documents);
+  res.status(200).json(client_documents.map(({ value }) => value));
 };
