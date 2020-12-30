7e7716551fb4fdd3772d875cd0a796e47f5282cc
Lefcott <leandro.cotti@widergy.com>
2020-12-29 21:47:42 -0300

    remove log

--- src/controllers/client_document.controller/list.js
+++ src/controllers/client_document.controller/list.js
@@ -3,7 +3,6 @@ import ClientDocument from '../../models/ClientDocument';
 /** @param {import('express').Request} req @param {import('express').Response} res */
 export default async ({ query }, res) => {
   const client_documents = await ClientDocument.search(query);
-  log(query);
 
   res.status(200).json(client_documents.map(({ value }) => value));
 };
