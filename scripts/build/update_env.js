22b602182f369354756b95f92a24b7c80bfff6a2
Lefcott <leandro.cotti@widergy.com>
2021-01-05 09:09:39 -0300

    update build

--- scripts/build/update_env.js
+++ scripts/build/update_env.js
@@ -6,10 +6,7 @@ export const updateEnv = () =>
   new Promise(resolve => {
     fs.unlink(`${projectDir}/src/env.json`, async error => {
       if (error) console.error(error);
-      const env = `{\n  ${Object.keys(process.env)
-        .map(key => `"${key}": "${process.env[key].replace(/\\/g, '\\\\').replace(/"/g, '\\"')}"`)
-        .join(',\n  ')}\n}\n`;
-      fs.writeFile(`${projectDir}/src/env.json`, env, writeError => {
+      fs.writeFile(`${projectDir}/src/env.json`, JSON.stringify(process.env), writeError => {
         if (writeError) console.error(writeError);
         resolve(true);
       });
