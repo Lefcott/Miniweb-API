614e0072bfaf9e572a7ab646a33bdbfaf552c90d
Lefcott <leandro.cotti@widergy.com>
2021-01-07 13:35:42 -0300

    move_headers

--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
   console.log('headers', req.headers);
 
   res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cookie');
   next();
 });
 
@@ -59,11 +60,6 @@ app.use((req, res, next) => {
 app.use(rollbar.errorHandler());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cookie');
-  next();
-});
 
 app.get('/', (req, res) => res.status(200).send('Hello World!'));
 app.use('/', router);
