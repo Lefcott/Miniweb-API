372715e177d645fb9fb4167cc235f1b9ff862ed4
Lefcott <leandro.cotti@widergy.com>
2021-01-09 20:03:20 -0300

    add default value

--- src/models/ClientModel.js
+++ src/models/ClientModel.js
@@ -15,6 +15,7 @@ const ClientModel = mongoose.model(
         {
           key: { type: String, required: true },
           name: { type: String, required: true },
+          default_value: { type: mongoose.SchemaTypes.Mixed },
           important: { type: Boolean, required: true },
           input_type: { type: String, required: true }, // text | number | image | email | phone
           is_required: { type: Boolean, default: false }
