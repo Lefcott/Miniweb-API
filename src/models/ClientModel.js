279f22cf5d4df7a25464edf283a111e415b91f71
Lefcott <leandro.cotti@widergy.com>
2021-01-08 22:56:21 -0300

    add name to client model

--- src/models/ClientModel.js
+++ src/models/ClientModel.js
@@ -8,6 +8,7 @@ const ClientModel = mongoose.model(
   'ClientModel',
   mongoose.Schema(
     {
+      name: { type: String, required: true },
       table_name: { type: String, required: true },
       table_descriptive_name: { type: String, required: true },
       fields: [
