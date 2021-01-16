4cce2589276adbc01838825f48cc407fee7badad
Lefcott <leandro.cotti@widergy.com>
2021-01-16 17:20:58 -0300

    add reference to field model

--- src/models/shared/Field.js
+++ src/models/shared/Field.js
@@ -6,7 +6,13 @@ const Field = {
   default_value: { type: mongoose.SchemaTypes.Mixed },
   important: { type: Boolean, required: true },
   input_type: { type: String, required: true }, // text | number | image | email | phone
-  is_required: { type: Boolean, default: false }
+  is_required: { type: Boolean, default: false },
+  references: [
+    {
+      form_code: { type: String, required: true },
+      field_key: { type: String, required: true }
+    }
+  ]
 };
 
 export default Field;
