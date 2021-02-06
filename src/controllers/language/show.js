f2476bf7f9e77fcc491087d5bb6fb695f4c48d6e
Lefcott <leandro.cotti@widergy.com>
2021-02-06 19:11:20 -0300

    fix

--- src/controllers/language/show.js
+++ src/controllers/language/show.js
@@ -2,7 +2,7 @@ import User from '../../models/User';
 
 /** @param {import('express').Request} req @param {import('express').Response} res */
 export default async ({ session }, res) => {
-  const code = session.language_code || 'es';
+  const code = session.language_code || 'en';
 
   res.json({ code });
 };
