b47fbc1264e76dc91d0157bbd939d4087282342a
Lefcott <leandro.cotti@widergy.com>
2021-02-12 01:15:23 -0300

    fix

--- src/utils/messages/slack/text.js
+++ src/utils/messages/slack/text.js
@@ -5,8 +5,7 @@ import { BASE_URL } from './constants';
 export const send_text_message = (headers, conversation, message) => {
   const body = {
     channel: conversation.id,
-    text: message.text,
-    parse_mode: 'Markdown'
+    text: message.text
   };
 
   return axios.post(BASE_URL, body, { headers });
