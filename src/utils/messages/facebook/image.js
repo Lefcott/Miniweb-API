64cdf69a752a2c808d2942ec08019551d5811d52
Lefcott <leandro.cotti@widergy.com>
2021-02-13 01:23:11 -0300

    add recipient to db image

--- src/utils/messages/facebook/image.js
+++ src/utils/messages/facebook/image.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const send_image_message = (url, conversation, message) => {
   const body = {
+    recipient: { id: conversation.id },
     attachment: {
       type: 'image',
       payload: { url: message.image_url, is_reusable: true }
