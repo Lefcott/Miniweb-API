fd8e3e1967eb2ff7ef682b6358f533b8426f29c8
Lefcott <leandro.cotti@widergy.com>
2021-02-23 03:07:00 -0300

    add cancel and success urls

--- src/controllers/payment_session/create.js
+++ src/controllers/payment_session/create.js
@@ -23,9 +23,9 @@ export default async ({ body, params }, res) => {
         quantity: 1
       }
     ],
-    mode: 'payment'
-    // success_url: `${YOUR_DOMAIN}/success.html`,
-    // cancel_url: `${YOUR_DOMAIN}/cancel.html`
+    mode: 'payment',
+    success_url: body.success_url,
+    cancel_url: body.cancel_url
   });
   res.json({ id: session.id });
 };
--- src/routes/ecommerce_api/payment_session.js
+++ src/routes/ecommerce_api/payment_session.js
@@ -2,5 +2,9 @@ import joi from '@hapi/joi';
 
 export const create = {
   method: 'post',
-  paths: '/products/:product_id/payment_sessions'
+  paths: '/products/:product_id/payment_sessions',
+  body: joi.object().keys({
+    success_url: joi.string().required(),
+    cancel_url: joi.string().required()
+  })
 };
