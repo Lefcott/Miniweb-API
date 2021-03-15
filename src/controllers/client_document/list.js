cc63fc9670deccf9aeb8076b63bb91ddd1a57e7b
Lefcott <leandro.cotti@widergy.com>
2021-03-15 01:08:59 -0300

    fix

--- src/controllers/client_document/list.js
+++ src/controllers/client_document/list.js
@@ -2,9 +2,10 @@ import ClientDocument from '../../models/ClientDocument';
 
 /** @param {import('express').Request} req @param {import('express').Response} res */
 export default async ({ query }, res) => {
+  const { count } = query;
   const client_documents = await ClientDocument.search(query);
 
-  if (query.count) return res.json({ count: client_documents });
+  if (count) return res.json({ count: client_documents });
 
   res.json(client_documents.map(client_document => client_document.sanitize()));
 };
