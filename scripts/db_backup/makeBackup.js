aae3834ea88fd13fd98c6228f3846c938553e7e4
updater <lefcott@hotmail.com>
2021-04-13 12:46:05 -0300

    fix

--- scripts/db_backup/makeBackup.js
+++ scripts/db_backup/makeBackup.js
@@ -7,11 +7,11 @@ export const makeBackup = async repoName =>
     mongoose.connection.on('open', () => {
       mongoose.connection.db.listCollections().toArray(async (err, collections) => {
         if (err) throw err;
-        if (!fs.existsSync(`${repoName}/database`)) fs.mkdirSync(`${repoName}/database`);
+        if (!fs.existsSync(`${repoName}/documents`)) fs.mkdirSync(`${repoName}/documents`);
         for (let i = 0; i < collections.length; i += 1) {
           const { name } = collections[i];
           const docs = await mongoose.connection.collection(name).find({}).toArray();
-          fs.writeFileSync(`${repoName}/database/${name}.json`, JSON.stringify(docs, null, 2));
+          fs.writeFileSync(`${repoName}/documents/${name}.json`, JSON.stringify(docs, null, 2));
           log(`wrote ${docs.length} documents in collection ${name}`);
         }
 
