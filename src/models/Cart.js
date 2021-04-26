faa8ff73278087ca874aefa063363fc3f2ddf226
updater <lefcott@hotmail.com>
2021-04-26 15:40:08 -0300

    add pcode and fcode

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -8,6 +8,8 @@ const CartBase = mongoose.model(
   'Cart',
   mongoose.Schema(
     {
+      project_code: { type: String, required: true },
+      form_code: { type: String, required: true },
       items: [
         {
           product: {},
@@ -22,6 +24,8 @@ const CartBase = mongoose.model(
 export default class Cart extends CartBase {
   static async create(body, params) {
     const cart = new Cart({
+      project_code: params.project_code,
+      form_code: params.form_code,
       items: body.items
     });
 
