4944ba51e00008ea062507baa3ef8c68c44dabf0
updater <lefcott@hotmail.com>
2021-05-06 12:45:08 -0300

    add token to sessions

--- src/controllers/session/create.js
+++ src/controllers/session/create.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken';
+
 import User from '../../models/User';
 
 /** @param {import('express').Request} req @param {import('express').Response} res */
@@ -6,8 +8,9 @@ export default async ({ params, body, session }, res) => {
 
   session.user_id = user._id;
   session.language_code = user.language_code;
+  const token = jwt.sign({ user_id: user._id }, process.env.TOKEN_SECRET);
 
   user.makeSecure();
 
-  res.json([{ user }]);
+  res.json([{ user, token }]);
 };
