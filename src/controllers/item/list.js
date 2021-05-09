b144e0bde0d4165394b85d5edffe6a4802592057
updater <lefcott@hotmail.com>
2021-05-08 22:07:50 -0300

    fix

--- src/controllers/item/list.js
+++ src/controllers/item/list.js
@@ -5,7 +5,7 @@ export default async ({ query }, res) => {
   const { count } = query;
   const items = await Item.search(query);
 
-  if (count) return res.json({ count: items[0]?.count });
+  if (count) return res.json({ count: (items[0] && items[0].count) || 0 });
 
   res.json(Item.sanitize_items(true, items));
 };
