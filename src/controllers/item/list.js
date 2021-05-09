ff846c3dc8008e7ed24f84720968cb7f18be54c0
updater <lefcott@hotmail.com>
2021-05-08 21:57:51 -0300

    add countt

--- src/controllers/item/list.js
+++ src/controllers/item/list.js
@@ -5,7 +5,7 @@ export default async ({ query }, res) => {
   const { count } = query;
   const items = await Item.search(query);
 
-  if (count) return res.json({ count: items });
+  if (count) return res.json({ count: items.count });
 
   res.json(Item.sanitize_items(true, items));
 };
