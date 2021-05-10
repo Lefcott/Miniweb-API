578a5c05b7622a3c371dd976dbd5b0388af23c05
updater <lefcott@hotmail.com>
2021-05-10 14:57:32 -0300

    comic effects

--- src/models/Item/effects/index.js
+++ src/models/Item/effects/index.js
@@ -4,22 +4,30 @@ export default [
   {
     project_code: 'readmyfcomic',
     entity: 'user',
-    apply: client_document => {
-      readmyfcomic.apply_complete_name(client_document);
-      readmyfcomic.apply_views_count(client_document);
-      readmyfcomic.apply_rating_average(client_document);
-      client_document.value.ratings = undefined;
-      client_document.value.views = undefined;
+    apply: item => {
+      readmyfcomic.apply_complete_name(item);
+      readmyfcomic.apply_views_count(item);
+      readmyfcomic.apply_rating_average(item);
+      item.value.ratings = undefined;
+      item.value.views = undefined;
+    }
+  },
+  {
+    project_code: 'readmyfcomic',
+    entity: 'comic',
+    apply: item => {
+      readmyfcomic.apply_views_count(item);
+      item.value.views = undefined;
     }
   },
   {
     project_code: 'readmyfcomic',
     entity: 'forum',
-    apply: client_document => {
-      readmyfcomic.apply_comments_count(client_document);
-      readmyfcomic.apply_views_count(client_document);
-      client_document.value.comments = undefined;
-      client_document.value.views = undefined;
+    apply: item => {
+      readmyfcomic.apply_comments_count(item);
+      readmyfcomic.apply_views_count(item);
+      item.value.comments = undefined;
+      item.value.views = undefined;
     }
   }
 ];
